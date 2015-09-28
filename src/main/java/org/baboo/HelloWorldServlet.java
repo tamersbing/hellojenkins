871e1f5b5c0fa5161bfc8903ba9763770eeb12d2
tamersbing <tamersbing@gmail.com>
2015-09-27 19:32:42 -0700

    maven updates

---
+++ src/main/java/org/baboo/HelloWorldServlet.java
@@ -0,0 +1,35 @@
+package org.baboo;
+
+// Import required java libraries
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+// Extend HttpServlet class
+public class HelloWorldServlet extends HttpServlet {
+ 
+  private String message;
+
+  public void init() throws ServletException
+  {
+      // Do required initialization
+      message = "Hello Jenkins, from servlet";
+  }
+
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+            throws ServletException, IOException
+  {
+      // Set response content type
+      response.setContentType("text/html");
+
+      // Actual logic goes here.
+      PrintWriter out = response.getWriter();
+      out.println("<h1>" + message + "</h1>");
+  }
+  
+  public void destroy()
+  {
+      // do nothing.
+  }
+}
