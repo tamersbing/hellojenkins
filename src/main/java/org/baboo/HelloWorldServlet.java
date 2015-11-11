5db2f2ea8a3b0772549a60dde3412e6e561254bc
tamersbing <tamersbing@gmail.com>
2015-11-11 08:26:02 -0800

    Hi GEHC

--- src/main/java/org/baboo/HelloWorldServlet.java
+++ src/main/java/org/baboo/HelloWorldServlet.java
@@ -14,7 +14,7 @@ public class HelloWorldServlet extends HttpServlet {
   public void init() throws ServletException
   {
       // Do required initialization
-      message = "Hello Jenkins, from servlet in Tomcat after autocompile and git update to github ... time for a demo for CP";
+      message = "Hello GEHC, from servlet in Tomcat after autocompile and git update to github ... time for a demo for CP";
   }
 
   public void doGet(HttpServletRequest request,
